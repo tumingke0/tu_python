c80785c9ab7136ea4014837fab55c14e071525c1
tumingke <tumingke@58pic.com>
2019-07-10 23:49:39 +0800

    安装及介绍

---
+++ 开发工具以及安装/hello_world.py
@@ -0,0 +1,11 @@
+"""
+一般情况，学习一门语言，写的第一行代码就是 输出hello,world, 我们学习python同样。
+1. 打开你的 PyCharm，右击 -> new -> python file
+2. 输入 print("Hello World")
+3. ctrl + shift +F10 或者右击 Run "hello_world"
+"""
+
+print('Hello, World')
+
+
+
